4a60e5e4b2c7a08620cc4e480108c2131e16b953
Roman Voitenko <roman.voitenko@konsult.atg.se>
2021-05-09 20:30:52 +0200

    better file names, separate alkalinity test

---
+++ cypress/integration/trident-alkalinity.js
@@ -0,0 +1,15 @@
+describe('Window', () => {
+    it('get the document object', () => {
+        const username = Cypress.env('username')
+        const password = Cypress.env('password')
+        cy.visit('https://apexfusion.com/login');
+        cy.get('#index-login-username').type(username);
+        cy.get('#index-login-password').type(password);
+        cy.get('fieldset').click();
+        cy.get('.btn-primary').click();
+        cy.get('#index-panel .card').submit();
+        cy.get('.lead').click();
+        cy.get('.dash-tri-config > .af').click();
+        cy.get('.dash-tri-config-single').click({ force: true });
+    })
+})
